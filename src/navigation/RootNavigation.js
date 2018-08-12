4d5eb67654eee4f0216ed5cd3b2976d012737620
andela-mabdussalam <mariam.abdussalam@andela.com>
2018-08-12 20:19:09 +0100

    first commit

---
+++ src/navigation/RootNavigation.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Easing, TouchableHighlight, Image, View } from 'react-native';
+import { StackNavigator, DrawerNavigator } from 'react-navigation';
+import { Icon } from 'native-base';
+import { NavigationStyles as styles } from "../styles";
+import {
+  HomeScreen,
+  NewRateScreen,
+  PrevRateScreen,
+  RateBoundariesScreen,
+  CreateUserScreen,
+  ManageUserScreen,
+  FAQScreen,
+  PostScheduleScreen,
+  UpdateScreen
+} from '../screens';
+import { StyledText as Text, SideMenu } from '../components';
+
+const drawerButton = navigation =>
+  <TouchableHighlight
+    underlayColor='#173D0C'
+    onPress={() => {
+      if (navigation.state.index === 0) {
+        navigation.navigate('DrawerOpen');
+      } else {
+        navigation.navigate('DrawerClose');
+      }
+    }
+    }>
+    <Icon style={styles.icon} name='ios-menu-outline' />
+  </TouchableHighlight>;
+
+export const DrawerStack = DrawerNavigator({
+  NewRate: { screen: NewRateScreen },
+  PrevRate: { screen: PrevRateScreen },
+  CreateUser: { screen: CreateUserScreen },
+  ManageUsers: { screen: ManageUserScreen },
+  Configuration: { screen: RateBoundariesScreen },
+  PostSchedule: { screen: PostScheduleScreen },
+  Update: { screen: UpdateScreen },
+  Help: { screen: FAQScreen },
+},
+  {
+    gesturesEnabled: false,
+    drawerWidth: 250,
+    contentComponent: SideMenu,
+    drawerBackgroundColor: '#004900'
+  });
+
+const DrawerNavigation = StackNavigator({
+  DrawerStack: { screen: DrawerStack },
+}, {
+    headerMode: 'float',
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: '#173D0C',
+        paddingLeft: 10,
+        height: 40
+      },
+      headerTitle: (
+        <View style={styles.headerTitleStyle}>
+          <Image
+            style={styles.imageStyle}
+            source={require('../../src/assets/images/logo.png')}
+          />
+          <View style={styles.bdcView}>
+            <Text style={styles.text}>BDC FX</Text>
+            <Text style={styles.text}>RATE ENGINE</Text>
+          </View>
+        </View>
+      ),
+      headerTintColor: 'white',
+      gesturesEnabled: false,
+      headerLeft: drawerButton(navigation),
+      headerTitleStyle: {
+        fontFamily: 'montserrat',
+        fontWeight: 'normal',
+      },
+    })
+  });
+
+const noTransitionConfig = () => ({
+  transitionSpec: {
+    duration: 0,
+    easing: Easing.step0
+  },
+});
+
+export const PrimaryNavigator = StackNavigator({
+  Main: { screen: HomeScreen },
+  DrawerStack: { screen: DrawerNavigation },
+}, {
+    // Default config for all screens
+    headerMode: 'none',
+    title: 'Main',
+    initialRouteName: 'Main',
+    transitionConfig: noTransitionConfig
+  });
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import { Dimensions, Keyboard } from 'react-native';
+import PropTypes from 'prop-types';
+import { reduxForm, destroy } from 'redux-form';
+import { Home } from '../components';
+import { multiSetItems } from '../util';
+const window = Dimensions.get('window');
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  static propTypes = {
+    input: PropTypes.object,
+    handleSubmit: PropTypes.func,
+    dispatch: PropTypes.func,
+    authenticateUserMutation: PropTypes.func,
+    navigation: PropTypes.object,
+    addTokenToStore: PropTypes.func,
+  }
+
+  state = {
+    showLogo: true,
+    visibleHeight: window.height,
+    errored: false,
+  };
+
+  componentDidMount() {
+    this.props.dispatch(destroy('login'));
+    this.keyboardWillShowSub = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow);
+    this.keyboardWillHideSub = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardWillShowSub.remove();
+    this.keyboardWillHideSub.remove();
+  }
+
+  keyboardDidShow = (e) => {
+    const newSize = window.height - (e.endCoordinates.height * 1.08);
+    this.setState((state) => ({
+      visibleHeight: newSize,
+      errored: false,
+      showLogo: false,
+      errorText: '',
+    }));
+  }
+
+  keyboardDidHide = () => {
+    this.setState((state) => ({
+      visibleHeight: window.height,
+      showLogo: true
+    }));
+  }
+
+  showError = (error) => {
+    if (JSON.stringify(error).includes('Invalid Credentials')) {
+      this.setState(() => ({
+        errored: true,
+        errorText: 'Incorrect Username or Password'
+      }));
+    } else if (JSON.stringify(error).includes('status code 400')) {
+      this.setState(() => ({
+        errored: true,
+        errorText: 'Provide Username and Password'
+      }));
+    } else {
+      this.setState(() => ({
+        errored: true,
+        errorText: 'Network error, retry'
+      }));
+    }
+  }
+
+  removeErrorText = () => {
+    this.setState(() => ({ errored: false }));
+  }
+
+  loginUser = async (data) => {
+    const { login: { token, user: { username, __typename } } } = data;
+    this.setState(() => ({ errored: false }));
+    const { navigate } = this.props.navigation;
+    await multiSetItems({ token, username, userType: __typename });
+    __typename === 'BDCAdmin' ? navigate('CreateUser') : navigate('DrawerStack');
+  }
+
+  render() {
+    const {
+      state: { showLogo, visibleHeight, errored, errorText },
+      props: { handleSubmit },
+      removeErrorText, showError, loginUser,
+    } = this;
+    return (
+      <Home
+        error={errored}
+        errorText={errorText}
+        handleSubmit={handleSubmit}
+        loginUser={loginUser}
+        removeErrorText={removeErrorText}
+        showError={showError}
+        showLogo={showLogo}
+        visibleHeight={visibleHeight}
+      />
+    );
+  }
+}
+
+const LoginForm = reduxForm({
+  form: 'login',
+})(HomeScreen);
+
+export default LoginForm;
