7c42321eabbd5a0b12238816b038e71261f1821c
oreoluwade <oreoluwade@gmail.com>
2018-09-06 10:14:04 +0100

    create CourseItem component

---
+++ src/components/CourseItem.js
@@ -0,0 +1,57 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import {
+  View,
+  ImageBackground,
+  Text,
+  Image,
+} from "react-native";
+import { CourseItemStyles as styles } from "../styles";
+
+class CourseItem extends PureComponent {
+  static propTypes = {
+    courseTitle: PropTypes.string.isRequired,
+    courseCount: PropTypes.number.isRequired,
+    courseIconStyle: PropTypes.object.isRequired,
+  }
+  render() {
+    const {
+      courseTitle,
+      courseCount,
+      courseIcon,
+      courseIconStyle
+    } = this.props;
+
+    const countSuffix = courseCount === 1 ? 'lesson' : 'lessons';
+
+    return (
+      <View style={styles.courseWrapper}>
+        <ImageBackground
+          style={styles.courseBox}
+          source={require("../assets/images/courseEllipse.png")}
+        >
+          <ImageBackground
+            style={styles.courseIconWrapper}
+            source={require("../assets/images/courseIconHolder.png")}
+            >
+            <Image
+              style={courseIconStyle}
+              source={courseIcon}
+              />
+          </ImageBackground>
+          <View style={styles.courseTextWrapper}>
+            <Text style={styles.courseTitleText}>{courseTitle}</Text>
+            <ImageBackground
+              style={styles.lessonCountHolder}
+              source={require("../assets/images/roundedRectangle.png")}
+            >
+              <Text style={styles.lessonCountText}>{`${courseCount} ${countSuffix}`}</Text>
+            </ImageBackground>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+export default CourseItem;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as SideMenu } from './SideMenu';
 export { default as Home } from './Home';
 export { default as ContentWrapper } from './ContentWrapper';
+export { default as CourseItem } from './CourseItem';
 export { default as RenderInput } from './RenderInput';
 export { default as MainMenu } from './MainMenu';
